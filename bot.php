453acbccb946678006067f4fb619519c3beece1c
amidfdssdfds <8vmfjadxq@30mail.ir>
2016-07-15 14:00:26 +0430

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,1 +1,23 @@
-<?php $update = json_decode(file_get_contents('php://input')); $message=$update->message->text; if($message=="سلام"){ $rep=json_decode(file_get_contents("https://api.telegram.org/bot201105405:AAHJ7ScIBhd8CCjZTDkrTY3vYgz10z4nSMg/SendMessage?chat_id=".$update->message->chat->id."&text=".urldecode("سلام. خوبی !؟"))); echo "Ok"; } ?>
+<?php
+$update = json_decode(file_get_contents('php://input'));
+$message=$update->message->text;
+$id=$update->message->message_id;
+if($message=="/randomgame"){
+    $file = fopen("var.txt","w");
+    echo fwrite($file,rand(1,100));
+    fclose($file);
+    $rep=json_decode(file_get_contents("https://api.telegram.org/bot198373457:AAGAgaKzrIz0p_Bk-i2G3H2kkk8u0KEsYuQ/SendMessage?chat_id=".$update->message->chat->id."&text=".urldecode("ye adad to zehname mitoni begi chane !? :D")));
+}else{
+    if((is_numeric($message)) && (filesize('var.txt') != 0) ){
+        if($message<file_get_contents('var.txt')){
+            $rep=json_decode(file_get_contents("https://api.telegram.org/bot198373457:AAGAgaKzrIz0p_Bk-i2G3H2kkk8u0KEsYuQ/SendMessage?chat_id=".$update->message->chat->id."&text=".urldecode("balatar")."&reply_to_message_id=".$id));
+        }elseif($message>file_get_contents('var.txt')){
+            $rep=json_decode(file_get_contents("https://api.telegram.org/bot198373457:AAGAgaKzrIz0p_Bk-i2G3H2kkk8u0KEsYuQ/SendMessage?chat_id=".$update->message->chat->id."&text=".urldecode("paintar")."&reply_to_message_id=".$id));
+        }elseif($message==file_get_contents('var.txt')){
+            $rep=json_decode(file_get_contents("https://api.telegram.org/bot198373457:AAGAgaKzrIz0p_Bk-i2G3H2kkk8u0KEsYuQ/SendMessage?chat_id=".$update->message->chat->id."&text=".urldecode("afarin doroste :)")."&reply_to_message_id=".$id));
+            file_put_contents("var.txt", "");
+        }
+    }
+}
+?>
+
